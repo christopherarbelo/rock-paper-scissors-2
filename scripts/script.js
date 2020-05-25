5f4c95c42111b3d357cd8ac08dc8b9946d0efaad
Christopher Arbelo <christopherarbelo@gmail.com>
2020-05-25 12:30:19 -0400

    Implement better UI with button selectability and output box

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,34 @@
+const scores = {
+    player: 0,
+    computer: 0,
+    ties: 0,
+    games: 0
+}
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => button.addEventListener('click', chooseSelection))
+
+function chooseSelection(e) {
+    const PARENT_TYPE = e.target.parentNode.type
+    let selection;
+
+    // checking to see PARENT_TYPE as a basis
+    // in selecting the textContent of the button
+    if (PARENT_TYPE === "button") {
+        selection = e.target.parentNode.children[1].textContent
+    } else {
+        selection = e.target.children[1].textContent
+    }
+
+    playRound(selection)
+}
+
 function computerPlay() {
     switch (getRandomNumber(1, 3)) {
         case 1:
             return "Rock"
         case 2:
-            return "Scissors"
+            return "Scissor"
         case 3:
             return "Paper"
         default:
@@ -12,80 +37,71 @@ function computerPlay() {
     }
 }
 
-function playRound() {
-    let playerSelection = prompt("Choose your pick")
-
-    while (!checkInput(playerSelection)) {
-        playerSelection = prompt("The choices are: Rock, Paper or Scissors...")
-    }
-
+function playRound(playerSelection) {
     let computerSelection = computerPlay()
-
-    playerSelection = formatSelection(playerSelection)
 
     switch (true) {
         case playerSelection === computerSelection:
-            console.log("TIE!")
+            updateScore("ties", "TIE!")
             break;
-        case playerSelection === "Rock" && computerSelection === "Scissors":
-            console.log("PLAYER WINS, ROCK BEATS SCISSORS!")
-            return "player"
+        case playerSelection === "Rock" && computerSelection === "Scissor":
+            updateScore("player", "PLAYER WINS, ROCK BEATS SCISSOR!")
+            break
         case playerSelection === "Rock" && computerSelection === "Paper":
-            console.log("Computer WINS, PAPER BEATS ROCK!")
-            return "computer"
-        case playerSelection === "Scissors" && computerSelection === "Paper":
-            console.log("PLAYER WINS, SCISSORS CUT PAPER!")
-            return "player"
-        case playerSelection === "Scissors" && computerSelection === "Rock":
-            console.log("Computer WINS, ROCK BEATS SCISSORS!")
-            return "computer"
+            updateScore("computer", "Computer WINS, PAPER BEATS ROCK!")
+            break
+        case playerSelection === "Scissor" && computerSelection === "Paper":
+            updateScore("player", "PLAYER WINS, SCISSOR CUT PAPER!")
+            break
+        case playerSelection === "Scissor" && computerSelection === "Rock":
+            updateScore("computer", "Computer WINS, ROCK BEATS SCISSOR!")
+            break
         case playerSelection === "Paper" && computerSelection === "Rock":
-            console.log("PLAYER WINS, PAPER TOPS ROCKS!")
-            return "player"
-        case playerSelection === "Paper" && computerSelection === "Scissors":
-            console.log("Computer WINS, SCISSORS CUT PAPER!")
-            return "computer"
+            updateScore("player", "PLAYER WINS, PAPER TOPS ROCKS!")
+            break
+        case playerSelection === "Paper" && computerSelection === "Scissor":
+            updateScore("computer", "Computer WINS, SCISSOR CUT PAPER!")
+            break
     }
-
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+function updateScore(winner, message) {
+    const GAMES = document.querySelector("#games")
+    const PLAYER_WINS = document.querySelector("#player-wins")
+    const COMPUTER_WINS = document.querySelector("#computer-wins")
+    const TIES = document.querySelector("#ties")
+    const DISPLAY = document.querySelector("#display")
+    const DISPLAY_MESSAGE = document.querySelector("#display p")
 
-    const roundOne = playRound()
-    updateScore(roundOne)
-    const roundTwo = playRound()
-    updateScore(roundTwo)
-    const roundThree = playRound()
-    updateScore(roundThree)
-    const roundFour = playRound()
-    updateScore(roundFour)
-    const roundFive = playRound()
-    updateScore(roundFive)
+    scores[winner]++
+    scores["games"]++
 
-    function updateScore(outcome) {
-        if (outcome === "computer") computerScore++
-        else if (outcome === "player") playerScore++
+    GAMES.textContent = `Games: ${scores["games"]}`
+    PLAYER_WINS.textContent = `Player Wins: ${scores["player"]}`
+    COMPUTER_WINS.textContent = `Computer Wins: ${scores["computer"]}`
+    TIES.textContent = `Ties: ${scores["ties"]}`
+
+    if (scores["games"] < 5) {
+        DISPLAY_MESSAGE.textContent = message
+    } else {
+        if (scores["computer"] > scores["player"]) {
+            DISPLAY_MESSAGE.textContent = "Computer wins SERIES!"
+        } else if (scores["computer"] < scores["player"]) {
+            DISPLAY_MESSAGE.textContent = "Player wins SERIES!"
+        } else {
+            DISPLAY_MESSAGE.textContent = "Wow a tie!"
+        }
+
+        buttons.forEach(button => button.setAttribute("disabled", ""))
     }
 
-    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`)
+    DISPLAY.style.cssText = "background: red;"
+
+    DISPLAY.addEventListener("transitionend", () => {
+        DISPLAY.style.background = "none";
+    })
 }
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min)) + min
-}
-
-function formatSelection(playerSelection) {
-    return playerSelection.charAt(0).toUpperCase() +
-        playerSelection.toLowerCase().slice(1)
-}
-
-function checkInput(input) {
-    input = input.toLowerCase()
-    if (input !== "rock" && input !== "paper" && input !== "scissors") {
-        return false
-    }
-
-    return true
 }
